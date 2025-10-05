ee39baa16229ae3124ffdb69ea38cd22bb1914aa
MerlanFriX <107347267+MerlanFriX@users.noreply.github.com>
2025-10-05 21:46:53 +0200

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,17 @@
+ACCOUNTS = {
+    "bitget1": {
+        "public_api": "",
+        "secret_api": "",
+        "password": "",
+    },
+    "bitget_main": {
+        "public_api": "",
+        "secret_api": "",
+        "password": "",
+    },
+    "bitmart1": {
+        "public_api": "",
+        "secret_api": "",
+        "memo": "",
+    },
+}
